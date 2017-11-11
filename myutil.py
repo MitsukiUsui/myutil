21a55b866aa9efac621c16b83ca946031f2919a9
Mitsuki Usui <mitsuki-usui@sango-login2.oist.jp>
2017-11-11 13:45:45 +0900

    init at sango

--- myutil.py
+++ myutil.py
@@ -10,3 +10,10 @@ def myrun(cmd):
         print(e)
         return False
     return True
+
+def create_direc(direc):
+    """
+    mkdir -p ${direc} 
+    """
+    cmd="mkdir -p {}".format(direc)
+    return myrun(cmd)
