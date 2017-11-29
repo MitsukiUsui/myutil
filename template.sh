27c703821dc6ee820fbc244c029ae3ee71cc4894
MitsukiUsui <mitsuo.com.mu@gmail.com>
2017-11-29 15:31:13 +0900

    debug array jobs

--- template.sh
+++ template.sh
@@ -13,18 +13,18 @@ fi
 # array job expander
 #--------------------------------------------------------------------------------
 argFilepath=hoge
-if [ -z ${SLURM_ARRAY_TASK_ID+x} ]; then lineNum=1; else lineNum=${SLURM_ARRAY_TASK_ID} fi;
+if [ -z ${SLURM_ARRAY_TASK_ID+x} ]; then lineNum=1; else lineNum=${SLURM_ARRAY_TASK_ID}; fi;
 line=`awk -v lineNum=$linuNum '{if (NR == lineNum) print $0}' ${argFilepath}`
 
 
 #--------------------------------------------------------------------------------
 # submit array jobs
 #--------------------------------------------------------------------------------
-cmd=demo.sh
+cmd= #WIRTE ME like examble.sh
 argCmd=./arg/${cmd/.sh/.py}
 argFilepath=./arg/${cmd/.sh/.list}
 eval ${argCmd} > ${argFilepath}
 numJobs=`grep -c '' ${argFilepath}`
-prevJobId= #WRITE ME
+prevJobId=${jobId}
 jobId=`sbatch --parsable --array=1-${numJobs} --dependency=afterok:${prevJobId} ${cmd} ${argFilepath}`
-echo "submitted "${numJobs}" jobs with job_id="${prevJobId}", dependency="${jobId}
+echo "submitted "${numJobs}" jobs with job_id="${jobId}", dependency="${prevJobId}
