436195d539f864850d5288a6fb111c2cc853841b
MitsukiUsui <mitsuo.com.mu@gmail.com>
2017-11-29 15:09:10 +0900

    add array job

--- template.sh
+++ template.sh
@@ -4,7 +4,7 @@
 FORCE_MODE=false
 forceFilepath=hoge
 if [ "$FORCE_MODE" = false ] && [ -e ${forceFilepath} ]; then
-    echo "PASS: target already distributed"
+    echo "PASS: target file already exists"
 else
     #WRITE ME
 fi
@@ -15,3 +15,16 @@ fi
 argFilepath=hoge
 if [ -z ${SLURM_ARRAY_TASK_ID+x} ]; then lineNum=1; else lineNum=${SLURM_ARRAY_TASK_ID} fi;
 line=`awk -v lineNum=$linuNum '{if (NR == lineNum) print $0}' ${argFilepath}`
+
+
+#--------------------------------------------------------------------------------
+# submit array jobs
+#--------------------------------------------------------------------------------
+cmd=demo.sh
+argCmd=./arg/${cmd/.sh/.py}
+argFilepath=./arg/${cmd/.sh/.list}
+eval ${argCmd} > ${argFilepath}
+numJobs=`grep -c '' ${argFilepath}`
+prevJobId= #WRITE ME
+jobId=`sbatch --parsable --array=1-${numJobs} --dependency=afterok:${prevJobId} ${cmd} ${argFilepath}`
+echo "submitted "${numJobs}" jobs with job_id="${prevJobId}", dependency="${jobId}
